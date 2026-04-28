784c41c833ee074bd70b5422fb8383b896d3aeda
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-04-28 09:07:49 -0500

    Create Main.java

---
+++ Practica 5/ACT/ACT01/Main.java
@@ -0,0 +1,8 @@
+
+public class Main{
+    public static void main(String[] args){
+        Node<Integer> nodo = new Node<>(42);
+        System.out.println(nodo.value);//42
+        System.out.println(nodo.next);//null
+    }
+} 
