9b3e988a080aac47cdc3fac9a24b0edc7052d18f
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-04-28 09:08:30 -0500

    Create Node.java

---
+++ Practica 5/EJER/EJER01/Node.java
@@ -0,0 +1,11 @@
+
+public class Node<T>{
+    //Atributos del nodo
+    T value; //almacena el dato del nodo
+    Node<T> next; //referencia al siguiente nodo en la lista
+    //Nuestro constructor
+    public Node(T value){
+        this.value = value;
+        this.next = null;
+    }
+}
