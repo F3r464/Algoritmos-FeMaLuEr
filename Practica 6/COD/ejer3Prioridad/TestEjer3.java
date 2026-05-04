fe0857e1c2e0849187d7a26d89fe0ff61cacfb4e
erica1415 <erica.alvarez2003@gmail.com>
2026-05-04 01:54:10 -0500

    Update TestEjer3.java

--- Practica 6/COD/ejer3Prioridad/TestEjer3.java
+++ Practica 6/COD/ejer3Prioridad/TestEjer3.java
@@ -2,14 +2,15 @@ package ejer3Prioridad;
 import actividad1.ExceptionIsEmpty;
 public class TestEjer3{
     public static void main(String[] args) throws ExceptionIsEmpty{
+        // Creamos una cola de prioridad con 3 niveles: 0 (baja), 1 (media), 2 (alta)
         ColaPrioridad<String> cp=new ColaPrioridad<>(3);
+        // Encolamos los elementos con sus respectivas prioridades
+        cp.enqueue("A",0);  //BAJA
+        cp.enqueue("B",2);  //ALTA
+        cp.enqueue("C",1);  //MEDIA
+        cp.enqueue("D",2);  //ALTA
 
-        cp.enqueue("A",0);
-        cp.enqueue("B",2);
-        cp.enqueue("C",1);
-        cp.enqueue("D",2);
-
-        while(!cp.isEmpty()){
+        while(!cp.isEmpty()){   // mientras la cola NO esté vacía
             System.out.println(cp.dequeue());
         }
     }
