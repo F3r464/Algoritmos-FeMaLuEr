5a1ab182921e9bb63e0f9e26d0decd5df858de60
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-06 18:03:04 -0500

    Update Cliente.java

--- Practica 6/Extra/Cliente.java
+++ Practica 6/Extra/Cliente.java
@@ -3,20 +3,16 @@ package Extra;
 public class Cliente {
     private String nombre;
     private String pedido;
-
     public Cliente(String nombre, String pedido) {
         this.nombre = nombre;
         this.pedido = pedido;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public String getPedido() {
         return pedido;
     }
-
     @Override
     public String toString() {
         return nombre + " pidió: " + pedido;
