bde1f31a896db0e243c30c77b0d4bdd6091e30aa
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-06 18:06:55 -0500

    Update Nodo.java

--- Practica 6/Extra/Nodo.java
+++ Practica 6/Extra/Nodo.java
@@ -1,22 +1,22 @@
 package Extra;
-public class Nodo<T> {
+public class Nodo<T>{
     private T dato;
     private Nodo<T> next;
 
-    public Nodo(T dato) {
+    public Nodo(T dato){
         this.dato = dato;
         this.next = null;
     }
-    public T getDato() {
+    public T getDato(){
         return dato;
     }
-    public void setDato(T dato) {
+    public void setDato(T dato){
         this.dato = dato;
     }
-    public Nodo<T> getNext() {
+    public Nodo<T> getNext(){
         return next;
     }
-    public void setNext(Nodo<T> next) {
+    public void setNext(Nodo<T> next){
         this.next = next;
     }
 }
