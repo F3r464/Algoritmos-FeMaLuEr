1b4584d5905bbf18b2add19d3e088ed17d6d2c88
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-11 18:50:06 -0500

    Create ItemNoFound.java

---
+++ Practica 7/ACT/ACT04/ItemNoFound.java
@@ -0,0 +1,10 @@
+package ACT.ACT04;
+
+public class ItemNoFound extends Exception{
+    public ItemNoFound(String msg){
+        super(msg);
+    }
+    public ItemNoFound(){
+        super();
+    }
+}
