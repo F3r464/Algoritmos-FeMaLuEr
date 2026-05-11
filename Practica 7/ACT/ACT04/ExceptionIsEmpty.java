e518c106e4b2d3181c1defb095bf40bbcc78e085
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-11 18:49:59 -0500

    Create ExceptionIsEmpty.java

---
+++ Practica 7/ACT/ACT04/ExceptionIsEmpty.java
@@ -0,0 +1,10 @@
+package ACT.ACT04;
+
+public class ExceptionIsEmpty extends Exception{
+    public ExceptionIsEmpty(String msg){
+        super(msg);
+    }
+    public ExceptionIsEmpty(){
+        super();
+    }
+}
