95f6c951faa7c6dd56ad3623190465788eeb5ca4
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-11 18:50:03 -0500

    Create ItemDuplicated.java

---
+++ Practica 7/ACT/ACT04/ItemDuplicated.java
@@ -0,0 +1,10 @@
+package ACT.ACT04;
+
+public class ItemDuplicated extends Exception{
+    public ItemDuplicated(String msg){
+        super(msg);
+    }
+    public ItemDuplicated(){
+        super();
+    }
+}
