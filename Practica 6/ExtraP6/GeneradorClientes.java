7177cd3dfca99587999c1b4ebcbf1a2f3be4f222
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-11 18:49:40 -0500

    Create GeneradorClientes.java

---
+++ Practica 6/ExtraP6/GeneradorClientes.java
@@ -0,0 +1,15 @@
+package ExtraP6;
+import java.util.Random;
+public class GeneradorClientes{
+    private static String[] nombres ={"p1", "p2", "p3", "p4", "p5"};
+    private static String[] pedidos ={"Hamburguesa", "Pizza", "Hot Dog", "Ensalada"};
+
+    public static Cliente generarCliente(){
+        Random rand = new Random();
+
+        String nombre = nombres[rand.nextInt(nombres.length)];
+        String pedido = pedidos[rand.nextInt(pedidos.length)];
+
+        return new Cliente(nombre, pedido);
+    }
+}
