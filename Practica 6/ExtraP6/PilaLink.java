5bb458df0b84bf47b35aa9e25257662f234d9b64
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-11 18:49:53 -0500

    Create PilaLink.java

---
+++ Practica 6/ExtraP6/PilaLink.java
@@ -0,0 +1,32 @@
+package ExtraP6;
+
+import ExtraP6.Nodo;
+
+public class PilaLink<T> implements Pila<T>{
+    private Nodo<T> cima;
+    public PilaLink(){
+        cima = null;
+    }
+    public void push(T dato){
+        Nodo<T> nuevo = new Nodo<>(dato);
+        nuevo.setNext(cima);
+        cima = nuevo;
+    }
+    public T pop() {
+        if (isEmpty()){
+            return null;
+        }
+        T dato = cima.getDato();
+        cima = cima.getNext();
+        return dato;
+    }
+    public T peek() {
+        if (isEmpty()){
+            return null;
+        }
+        return cima.getDato();
+    }
+    public boolean isEmpty(){
+        return cima == null;
+    }
+}
