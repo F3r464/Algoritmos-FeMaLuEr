f335ec7c89ce333e5e47ee3595c3860488181bf5
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-11 18:51:36 -0500

    Create Main.java

---
+++ Practica 7/ExtraP7/Main.java
@@ -0,0 +1,9 @@
+package ExtraP7;
+
+public static void main(String[] args) {
+    
+    SwingUtilities.invokeLater(() -> {
+        VisualizadorBST ventana = new VisualizadorBST();
+        ventana.setVisible(true); 
+    });
+}
