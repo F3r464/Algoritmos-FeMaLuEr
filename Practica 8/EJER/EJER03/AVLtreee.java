adb56207a1e331cfd9ab03101f5cc7a930e07c8d
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-05-23 13:51:37 -0500

    Update AVLtreee.java

--- Practica 8/EJER/EJER03/AVLtreee.java
+++ Practica 8/EJER/EJER03/AVLtreee.java
@@ -2,73 +2,156 @@ package EJER.EJER03;
 
 import ACT.ACT03.AVLtree; 
 import ACT.ACT04.*;
-import ACT.ACT06.*;
-
 public class AVLtreee<E extends Comparable<E>> extends AVLtree<E>{
-
+    private boolean altura;
     public void delete(E dato) throws ExceptionIsEmpty{
-        if (isEmpty()) {
+        if(isEmpty()){
             throw new ExceptionIsEmpty();
         }
+        altura = false;
         root = deleteAVL(root, dato);
     }
-
-    private Node deleteAVL(Node actual, E dato){
+    private AVLnodo deleteAVL(AVLnodo actual, E dato){
         if(actual == null){
             return null;
         }
+        int cmp=dato.compareTo(actual.dato);
 
-        int cmp = dato.compareTo(actual.dato);
-
-        if (cmp < 0){
-            actual.left = deleteAVL(actual.left, dato);
-        }else if (cmp > 0){
-            actual.right = deleteAVL(actual.right, dato);
-        } else {
-            if (actual.left == null){
+        if(cmp<0){
+            actual.left=deleteAVL(actual.left, dato);
+            if(altura){
+                actual=recargarBalanceDerecha(actual);
+            }
+        }else if(cmp>0){
+            actual.right=deleteAVL(actual.right, dato);
+            if(altura){
+                actual = recargarBalanceIzquierda(actual);
+            }
+        }else{
+            if(actual.left == null){
+                altura = true;
                 return actual.right;
             }
-            if (actual.right == null){
+            if(actual.right == null){ 
+                altura = true;
                 return actual.left;
             }
-            E sucesor = findMin(actual.right);
-            actual.dato = sucesor;
-            actual.right = deleteAVL(actual.right, sucesor);
+            E sucesor =findMin(actual.right);
+            actual.dato=sucesor;
+            actual.right =deleteAVL(actual.right, sucesor);
+            if(altura){
+                actual=recargarBalanceIzquierda(actual);
+            }
         }
-        ((AVLNode) actual).height = 1 + Math.max(height(actual.left), height(actual.right));
-
-        int bal = balance(actual);
-
-        // Caso LL
-        if (bal > 1 && balance(actual.left) >= 0){
-            return rotRight(actual);
-        }
-
-        // Caso LR
-        if (bal > 1 && balance(actual.left) < 0){
-            actual.left = rotLeft(actual.left);
-            return rotRight(actual);
-        }
-
-        // Caso RR
-        if (bal < -1 && balance(actual.right) <= 0){
-            return rotLeft(actual);
-        }
-
-        // Caso RL
-        if (bal < -1 && balance(actual.right) > 0){
-            actual.right = rotRight(actual.right);
-            return rotLeft(actual);
-        }
-
         return actual;
     }
-
-    private E findMin(Node node){
-        if (node.left == null){
+    private E findMin(AVLnodo node){
+        if(node.left==null){
             return node.dato;
-        } else {
+        }else{
             return findMin(node.left);
+        }
+    }
+    private AVLnodo recargarBalanceDerecha(AVLnodo node){
+        switch (node.bf){
+            case -1:
+                node.bf = 0;
+                altura = true;
+                break;
+            case 0: 
+                node.bf = 1;
+                altura = false;
+                break;
+            case 1: 
+                node = balanceToRightDelete(node);
+                break;
+        }
+        return node;
+    }
+    private AVLnodo recargarBalanceIzquierda(AVLnodo node){
+        switch (node.bf) {
+            case 1: 
+                node.bf = 0;
+                altura = true;
+                break;
+            case 0: 
+                node.bf = -1;
+                altura = false;
+                break;
+            case -1:
+                node = balanceToLeftDelete(node);
+                break;
+        }
+        return node;
+    }
+    private AVLnodo balanceToRightDelete(AVLnodo node){
+        AVLnodo der = node.right;
+        if(der.bf >= 0){
+            if (der.bf==0){
+                node.bf=1;
+                der.bf=-1;
+                altura=false;
+                return rotLeft(node);
+            }
+            node.bf=0;
+            der.bf=0;
+            altura= true;
+            return rotLeft(node);
+        }else{
+            AVLnodo izq=der.left;
+            switch (izq.bf){
+                case 1:  
+                    node.bf=-1;
+                    der.bf=0; 
+                    break;
+                case 0: 
+                    node.bf=0;  
+                    der.bf =0;
+                    break;
+                case -1:   
+                    node.bf=0;   
+                    der.bf=1;
+                    break;
+            }
+            izq.bf = 0;
+            node.right = rotRight(der);
+            altura = true;
+            return rotLeft(node);
+        }
+    }
+    private AVLnodo balanceToLeftDelete(AVLnodo node){
+        AVLnodo izq = node.left;
+        if (izq.bf <= 0){
+            if (izq.bf == 0){
+                node.bf = -1;
+                izq.bf = 1;
+                altura = false;
+                return rotRight(node);
+            }
+            node.bf=0;
+            izq.bf=0;
+            altura=true;
+            return rotRight(node);
+        } else {
+            AVLnodo der = izq.right;
+            switch (der.bf) {
+                case -1: 
+                    node.bf=1;  
+                    izq.bf=0; 
+                    break;
+                case 0:  
+                    node.bf=0; 
+                    izq.bf =0; 
+                    break;
+                case 1:  
+                    node.bf=0;  
+                    izq.bf=-1; 
+                    break;
+            }
+            der.bf = 0;
+            node.left = rotLeft(izq);
+            altura = true;
+            return rotRight(node);
         }
     }
 }
