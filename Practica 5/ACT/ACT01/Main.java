3f58537fd89bf098f6bf27e12cbe6ea07151ef44
F3r464 <f3rn4nd020w14infinite@gmail.com>
2026-06-04 20:16:27 -0500

    Delete Main.java

--- Practica 5/ACT/ACT01/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main{
-    public static void main(String[] args){
-        Node<Integer> nodo = new Node<>(42);
-        System.out.println(nodo.value);//42
-        System.out.println(nodo.next);//null
-    }
-} 
